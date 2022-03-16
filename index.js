91bd7956c1430e6b692706716fc933d3c13f40dd
Ryan Welcher <ryan.welcher@automattic.com>
2022-03-16 09:24:01 -0400

    Add copy php flag to commands.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ module.exports = {
         folderName: "src",
         editorScript: 'file:./index.js',
         editorStyle: 'file:./index.css',
-        style: 'file:./style-index.css'
+        style: 'file:./style-index.css',
+        customScripts: {
+            'start': 'wp-scripts start --webpack-copy-php',
+            'build': 'wp-scripts build --webpack-copy-php'
+        }
     },
     templatesPath: join( __dirname, 'plugin' ),
     blockTemplatesPath: join( __dirname, 'block' ),
